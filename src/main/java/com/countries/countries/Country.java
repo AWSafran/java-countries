ade595c66f06adf7b26c693f36f0720f4d0c48ce
AWSafran <andrewsafran@gmail.com>
2019-04-29 15:50:11 -0400

    created country class

--- src/main/java/com/countries/countries/Country.java
+++ src/main/java/com/countries/countries/Country.java
@@ -2,4 +2,56 @@ package com.countries.countries;
 
 public class Country
 {
+    private String name;
+    private long population;
+    private long size;
+    private int age;
+    
+    public Country(String name, long population, long size, int age)
+    {
+        this.name = name;
+        this.population = population;
+        this.size = size;
+        this.age = age;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public long getPopulation()
+    {
+        return population;
+    }
+    
+    public void setPopulation(long population)
+    {
+        this.population = population;
+    }
+    
+    public long getSize()
+    {
+        return size;
+    }
+    
+    public void setSize(long size)
+    {
+        this.size = size;
+    }
+    
+    public int getAge()
+    {
+        return age;
+    }
+    
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
 }
